0622afaa2e91a3a16ef4d40c75371ff3d3e37ec7
Alan <shenjunjie27@126.com>
2020-03-20 14:53:58 +0800

    ToString

--- module1/src/com/roob/module1/date320/ToString1.java
+++ module1/src/com/roob/module1/date320/ToString1.java
@@ -1,7 +1,7 @@
 package com.roob.module1.date320;
 
-public class HelloWorld {
+public class ToString1 {
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+
     }
 }
