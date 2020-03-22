d7573b652768c01c6b9858e16e11224e6d7c777b
Alan <shenjunjie27@126.com>
2020-03-22 19:47:00 +0800

    @Override     public String toString() {         return "Student{" +                 "name='" + name + '\'' +                 ", age=" + age +                 '}';     }
        重写了toString

--- module1/src/com/roob/module1/date321/HashSetDemo2.java
+++ module1/src/com/roob/module1/date321/HashSetDemo2.java
@@ -1,6 +1,7 @@
 package com.roob.module1.date321;
 
 import java.util.HashSet;
+import java.util.Iterator;
 
 //Set接口
 // 1.不允许存储重复的元素
@@ -10,16 +11,23 @@ import java.util.HashSet;
 public class HashSetDemo2 {
     public static void main(String[] args) {
         HashSet<Student> studentHashSet = new HashSet<Student>();
-        Student student=new Student("于谦",43);
+        Student student = new Student("于谦", 43);
         studentHashSet.add(student);
         studentHashSet.add(new Student("郭德纲", 44));
         studentHashSet.add(new Student("于谦", 43));
         studentHashSet.add(new Student("郭麒麟", 23));
         studentHashSet.add(student);
 
-        for(Student student2:studentHashSet){
+        for (Student student2 : studentHashSet) {//增强型for循环遍历
             System.out.println(student2);
         }
+
+//        Iterator<Student> it= studentHashSet.iterator();//迭代器遍历
+//        while (it.hasNext()){
+//            System.out.println(it.next());
+//        }
+
+        System.out.println(studentHashSet);
 
     }
 }
--- module1/src/com/roob/module1/date321/Student.java
+++ module1/src/com/roob/module1/date321/Student.java
@@ -6,6 +6,14 @@ public class Student {
     private String name;
     private int age;
 
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
     public Student() {
     }
 
